7443248886ec52be8238b881c74a45d0b7b0cc7b
tymor4098-cloud <tymor4098@gmail.com>
2026-08-29 01:57:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -90,9 +90,30 @@ function checkout(){
     return;
   }
 
-  alert("Checkout is ready.");
-}
+  const total = cart.reduce((sum, item) => {
+    return sum + (item.price * item.qty);
+  }, 0);
 
+  const popup = new Paystack();
+
+  popup.checkout({
+    key: 'pk_test_30ed6c6e03007391fcf23094d959f976896aba79,
+    email: 'customer@example.com',
+    amount: Math.round(total * 100),
+    currency: 'USD',
+
+    onSuccess: function(transaction){
+      alert(
+        "Payment successful! Reference: " +
+        transaction.reference
+      );
+    },
+
+    onCancel: function(){
+      alert("Payment cancelled.");
+    }
+  });
+}
 document.querySelectorAll(".filter").forEach(b=>{
   b.addEventListener("click",()=>{
     document.querySelectorAll(".filter")
